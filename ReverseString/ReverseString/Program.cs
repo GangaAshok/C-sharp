0b89e563f666345bff6cc182f97085971b96721f
GangaAshok <ganga.ashok@neudesic.com>
2019-06-20 23:33:03 +0530

    initial commit

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ReverseString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string str = "", reverse = "";
+            //    Console.WriteLine("Please input the string to be reversed");
+            //    str = Console.ReadLine();
+            //    var length = str.Length - 1;
+            //    while (length >= 0)
+            //    {
+            //        reverse = reverse + str[length];
+            //        length--;
+            //    }
+            //    Console.WriteLine("The output is " + reverse);
+            //    Console.ReadLine();
+            //}
+            //------------------------------------------------------------------------Approach 1
+            //    string str = "Abc"; 
+            Console.WriteLine("Enter the number to be reversed");
+            String str = Console.ReadLine();
+            Program program = new Program();
+            string result = program.reversedString(str);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+        public string reversedString(string str)
+        {
+            string reverse = "";
+            for (int i = str.Length; i > 0; i--)
+            {
+                reverse = reverse + str[i - 1];
+
+            }
+
+            return reverse;
+        }
+        //------------------------------------------Approach 2
+    }
+}
---
+++ ReverseString/StackArragements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace StackArragements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first digit you need to input");
+            int a = Int32.Parse(Console.ReadLine());
+            Stack st = new Stack();
+            st.Push(a);
+           Console.WriteLine("1 - push the element x into the stack ,2 - delete the element present at the top of the stack 3 - print the maximum element in the stack.");
+            Console.WriteLine("enter the option");
+            int option = Int32.Parse(Console.ReadLine());
+            Program program = new Program();
+            int value = program.getSelectedOption(st,option);
+
+        }
+
+       public int getSelectedOption(Stack st,int option)
+        {
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the digit you need to input");
+                    int b = Int32.Parse(Console.ReadLine());
+                    return st.Push(b);
+                default:
+                    return null;
+                    break;
+            }
+        }
+    }
+}
